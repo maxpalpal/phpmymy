70768d8885a328b30b92b846721d54e38ca5bf27
palestinianpy@gmail.com <palestinianpy@gmail.com>
2017-01-30 19:24:15 +0200

    s

--- 4455.php
+++ 4455.php
@@ -5,24 +5,7 @@
 
 require_once "Mail.php";
 
-$from = '<maxpen652@gmail.com>'; //change this to your email address
-$to = '<jihadralghoul@gmail.com>'; // change to address
-$subject = 'Insert subject here'; // subject of mail
-$body = "Hello world! this is the content of the email"; //content of mail
 
-$headers = array(
-    'From' => $from,
-    'To' => $to,
-    'Subject' => $subject
-);
-
-$smtp = Mail::factory('smtp', array(
-        'host' => 'ssl://smtp.gmail.com',
-        'port' => '465',
-        'auth' => true,
-        'username' => 'maxpen652@gmail.com', //your gmail account
-        'password' => '652zx652' // your password
-    ));
 
 // Send the mail
 $mail = $smtp->send($to, $headers, $body);
