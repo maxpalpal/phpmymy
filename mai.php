dc74fc015afad9ef1920613e97f399db82d1e40c
palestinianpy@gmail.com <palestinianpy@gmail.com>
2017-01-31 20:11:30 +0200

    dd

--- mai.php
+++ mai.php
@@ -1,27 +1,22 @@
-
 <?php
+// using SendGrid's PHP Library
+// https://github.com/sendgrid/sendgrid-php
 // If you are using Composer (recommended)
 require 'vendor/autoload.php';
-
-
-require("sendgrid-php.php");
-
 // If you are not using Composer
 // require("path/to/sendgrid-php/sendgrid-php.php");
-
-$from = new SendGrid\Email(null, "maxpen652@gmail.com");
-$subject = "Hope with thout";
-$to = new SendGrid\Email(null, "jihadralghoul@gmail.com");
-$content = new SendGrid\Content("text/plain", "Hello, Email!");
+$from = new SendGrid\Email("Example User", "king0xx@gmail.com");
+$subject = "Sending with SendGrid is Fun";
+$to = new SendGrid\Email("Example User", "jihadralghoul@gmail.com");
+$content = new SendGrid\Content("text/plain", "and easy to do anywhere, even with PHP");
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
-
-$apiKey = getenv('jbTaakLnSkaKmVzTtFpA5w');
+$apiKey = getenv('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');
 $sg = new \SendGrid($apiKey);
-
 $response = $sg->client->mail()->send()->post($mail);
 echo $response->statusCode();
 echo $response->headers();
 echo $response->body();
 
 
-?>
+
+
