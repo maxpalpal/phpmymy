30d00727be30a8b224ccaa32e9ab1b09fd85693a
palestinianpy@gmail.com <palestinianpy@gmail.com>
2017-01-31 19:54:29 +0200

    dd

--- mai.php
+++ mai.php
@@ -3,19 +3,19 @@
 // If you are using Composer (recommended)
 require 'vendor/autoload.php';
 
-require("SendGrid.php");
+
 require("sendgrid-php.php");
 
 // If you are not using Composer
 // require("path/to/sendgrid-php/sendgrid-php.php");
 
 $from = new SendGrid\Email(null, "maxpen652@gmail.com");
-$subject = "Hello World from the SendGrid PHP Library!";
+$subject = "Hope with thout";
 $to = new SendGrid\Email(null, "jihadralghoul@gmail.com");
 $content = new SendGrid\Content("text/plain", "Hello, Email!");
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
-$apiKey = getenv('3NsTabnaTWq166BlhDWsgQ');
+$apiKey = getenv('jbTaakLnSkaKmVzTtFpA5w');
 $sg = new \SendGrid($apiKey);
 
 $response = $sg->client->mail()->send()->post($mail);
