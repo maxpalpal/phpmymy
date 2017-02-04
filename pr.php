9819bbed1eb6e73263ef2dcac74d9fc9cb9c02df
maxpalpal <palestinianpy@gmail.com>
2017-02-05 00:56:22 +0200

    Update pr.php

---
+++ pr.php
@@ -0,0 +1,21 @@
+<?php
+require 'vendor/autoload.php';
+use SparkPost\SparkPost;
+use GuzzleHttp\Client;
+use Http\Adapter\Guzzle6\Client as GuzzleAdapter;
+ 
+$httpClient = new GuzzleAdapter(new Client());
+$sparky = new SparkPost($httpClient, ['key' => '900e849677118183375e843b9f679dff007c8523']);
+ 
+$sparky->setOptions(['async' => false]);
+$results = $sparky->transmissions->post([
+  'content' => [
+    'from' => 'heroku-node-js-example@sparkpostbox.com',
+    'subject' => 'Oh hey!',
+    'html' => '<html><body><p>Testing SparkPost - the world\'s most awesomest email service!</p></body></html>'
+  ],
+  'recipients' => [
+    ['address' => ['email'=>'maxpen652@gmail.com']]
+  ]
+]);
+?>
