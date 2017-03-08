4cc5b9ec55f02e34a863503d5ce211b0b9e467ad
maxpalpal <palestinianpy@gmail.com>
2017-03-08 17:59:07 +0200

    Update psr.php

--- psr.php
+++ psr.php
@@ -1,12 +1,13 @@
-<?
+<?php
 
 $subject = "sdsdsd";
 $msg = "love";
+$p = "222222";
 mysql_connect("us-cdbr-iron-east-03.cleardb.net","bca0357f3e762d","7728ad55");
 mysql_select_db("heroku_1b53b10aea6db1b") ;
 
 
-$query = "insert into user (username,email) values ('$subject','$msg')";
+$query = "insert into user (username,email,password) values ('$subject','$msg','$p')";
  mysql_query($query);
 
 
