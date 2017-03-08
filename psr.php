3e559d401f481f0f932af933ee92042882301039
maxpalpal <palestinianpy@gmail.com>
2017-03-08 19:07:36 +0200

    Update psr.php

--- psr.php
+++ psr.php
@@ -6,7 +6,7 @@ $subject = "sdsdsd";
 $msg = "love";
 $con = pg_connect('host=us-cdbr-iron-east-03.cleardb.net user=bca0357f3e762d password=b4fc873362e82f6a785efaaf9439b5a6bb214ce33a4a71444058f4ce4f1ee92d dbname=heroku_1b53b10aea6db1b connect_timeout=5'); 
  
-$query = " INSERT INTO public.man(
+$query = " INSERT INTO man(
 	username, password)
 	VALUES (2,3)"; 
 
