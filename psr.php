df93a9d853f9120f16f2472ac3e8a67c5a57c5c7
maxpalpal <palestinianpy@gmail.com>
2017-03-08 20:50:27 +0200

    Update psr.php

--- psr.php
+++ psr.php
@@ -6,9 +6,10 @@ mysql_connect("y0nkiij6humroewt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","mz7y3
 mysql_select_db("fd54ymxjciorkgy4") ;
 
 
-$query = " INSERT INTO fd54ymxjciorkgy4.hope(name)
+$query = ' INSERT INTO `fd54ymxjciorkgy4`.`hope`
+(`name`)
 VALUES
-('$msg') ";
+("jjj")';
  mysql_query($query);
 
 
