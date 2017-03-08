243f827ce159b2b8cedbdbc11244924af8a03f43
maxpalpal <palestinianpy@gmail.com>
2017-03-08 19:27:19 +0200

    Update psr.php

--- psr.php
+++ psr.php
@@ -4,17 +4,13 @@
 
 $subject = "sdsdsd";
 $msg = "love";
-$con = pg_connect("host=ec2-54-204-32-145.compute-1.amazonaws.com port=5432 dbname=dc0lv0spd5muji user=wulcydgyttdqij password=b4fc873362e82f6a785efaaf9439b5a6bb214ce33a4a71444058f4ce4f1ee92d");
+$con = pg_connect("host=ec2-54-204-32-145.compute-1.amazonaws.com port=5432 dbname=dc0lv0spd5muji user=wulcydgyttdqij password=b4fc873362e82f6a785efaaf9439b5a6bb214ce33a4a71444058f4ce4f1ee92d sslmode=require options='--client_encoding=UTF8' ");
 
 
 
 pg_query($con) or die("Cannot execute query");
 
 pg_close($con); 
-
-
-
-
 
 
 ?>
