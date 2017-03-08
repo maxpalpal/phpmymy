703033f7b55fcbf9561041a96b42f80a2cc350d9
maxpalpal <palestinianpy@gmail.com>
2017-03-08 19:02:06 +0200

    Update psr.php

--- psr.php
+++ psr.php
@@ -6,9 +6,9 @@ $subject = "sdsdsd";
 $msg = "love";
 $con = pg_connect('host=us-cdbr-iron-east-03.cleardb.net user=bca0357f3e762d password=b4fc873362e82f6a785efaaf9439b5a6bb214ce33a4a71444058f4ce4f1ee92d dbname=heroku_1b53b10aea6db1b connect_timeout=5'); 
  
-$query = "INSERT INTO public.man(
+$query = " INSERT INTO public.man(
 	username, password)
-	VALUES ('$subject','$msg')"; 
+	VALUES (2,3)"; 
 
 
 pg_query($con, $query) or die("Cannot execute query: $query\n");
