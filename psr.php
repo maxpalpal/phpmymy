4da0e4903609b2c21053ba8fc56c540e91b7180a
maxpalpal <palestinianpy@gmail.com>
2017-03-08 20:09:30 +0200

    Update

--- psr.php
+++ psr.php
@@ -6,8 +6,7 @@ mysql_connect("y0nkiij6humroewt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","mz7y3
 mysql_select_db("fd54ymxjciorkgy4") ;
 mysql_query("set names 'utf8'");
 
-$query = " INSERT INTO `fd54ymxjciorkgy4`.`hope`
-(`name`)
+$query = " INSERT INTO fd54ymxjciorkgy4.hope(name)
 VALUES
 ('$msg') ";
  mysql_query($query);
