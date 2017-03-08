513cef40ed880a13d4237f97b18c1a4c9d48b98c
maxpalpal <palestinianpy@gmail.com>
2017-03-08 19:47:11 +0200

    Update psr.php

--- psr.php
+++ psr.php
@@ -6,7 +6,7 @@ $subject = "sdsdsd";
 $msg = "love";
 $con = pg_connect("host=ec2-54-204-32-145.compute-1.amazonaws.com port=5432 dbname=dc0lv0spd5muji user=wulcydgyttdqij password=b4fc873362e82f6a785efaaf9439b5a6bb214ce33a4a71444058f4ce4f1ee92d sslmode=require options='--client_encoding=UTF8' ");
 
-$query = "INSERT INTO man(id, subject, msg)VALUES ( 7, '$subject', '$msg')"; 
+$query = "INSERT INTO max.user(id, subject, msg)VALUES ( 7, '$subject', '$msg')"; 
 pg_query($con, $query) or die("Cannot execute query: $query\n");
 
 
