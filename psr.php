9893517a3cc100d15bf13f6103387e5a353bfb90
maxpalpal <palestinianpy@gmail.com>
2017-03-08 20:02:40 +0200

    Update psr.php

--- psr.php
+++ psr.php
@@ -6,7 +6,7 @@ mysql_connect("y0nkiij6humroewt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","mz7y3
 mysql_select_db("fd54ymxjciorkgy4") ;
 mysql_query("set names 'utf8'");
 
-$query = "insert into hope (name) values ('sdsd')";
+$query = "insert into hope (name) values (10)";
  mysql_query($query);
 
 
