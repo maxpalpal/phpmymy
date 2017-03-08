731b1d94560ae4b58f01d2096331cece10f05aaa
maxpalpal <palestinianpy@gmail.com>
2017-03-08 19:15:22 +0200

    Update psr.php

--- psr.php
+++ psr.php
@@ -6,7 +6,7 @@ $subject = "sdsdsd";
 $msg = "love";
 $con = pg_connect("host=ec2-54-204-32-145.compute-1.amazonaws.com port=5432 dbname=dc0lv0spd5muji user=wulcydgyttdqij password=b4fc873362e82f6a785efaaf9439b5a6bb214ce33a4a71444058f4ce4f1ee92d sslmode=require options='--client_encoding=UTF8'") or die('Could not connect: ' . pg_last_error()); 
   
-$query = " INSERT INTO man(username, password)
+$query = " INSERT INTO public.man (username, password)
 	VALUES (2,3)"; 
 
 
