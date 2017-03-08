cbc027454fb5c53ec2d4ab9ee71d6a25c9c62451
maxpalpal <palestinianpy@gmail.com>
2017-03-08 17:56:37 +0200

    Update and rename pr.php to psr.php

--- psr.php
+++ psr.php
@@ -4,7 +4,7 @@ $subject = "sdsdsd";
 $msg = "love";
 mysql_connect("us-cdbr-iron-east-03.cleardb.net","bca0357f3e762d","7728ad55");
 mysql_select_db("heroku_1b53b10aea6db1b") ;
-mysql_query("set names 'utf8'");
+
 
 $query = "insert into user (username,email) values ('$subject','$msg')";
  mysql_query($query);
