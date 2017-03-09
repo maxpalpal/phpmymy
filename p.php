82c8e876a452203d05d395a1733a2c7814ab928c
maxpalpal <palestinianpy@gmail.com>
2017-03-09 12:09:15 +0200

    Update p.php

--- p.php
+++ p.php
@@ -1,5 +1,9 @@
-<?php 
-echo "kiad";
 
-
+<?php
+$link = mysql_connect('localhost', 'mysql_user', 'mysql_password');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connected successfully';
+mysql_close($link);
 ?>
