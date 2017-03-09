163f67316f411211fd8139870812caefea288904
maxpalpal <palestinianpy@gmail.com>
2017-03-09 12:04:34 +0200

    Update psr.php

--- psr.php
+++ psr.php
@@ -4,6 +4,7 @@ $subject = "sdsdsd";
 $msg = "love";
 mysql_connect("y0nkiij6humroewt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","mz7y3tb1ahil8uyc","nk9hj09j0ywgzrac");
 mysql_select_db("fd54ymxjciorkgy4") ;
+echo "sd";
 
 
 $query = ' INSERT INTO `fd54ymxjciorkgy4`.`hope`
@@ -20,3 +21,4 @@ echo "sd";
 
 
 ?>
+33333333333333333
