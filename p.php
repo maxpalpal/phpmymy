cdf0b9d736833bae6fcba21193ca54669875f37b
maxpalpal <palestinianpy@gmail.com>
2017-03-09 12:06:09 +0200

    Update p.php

--- p.php
+++ p.php
@@ -1,20 +1,4 @@
-<?
-
-$subject = "sdsdsd";
-$msg = "love";
-mysql_connect("y0nkiij6humroewt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","mz7y3tb1ahil8uyc","nk9hj09j0ywgzrac");
-mysql_select_db("fd54ymxjciorkgy4") ;
-echo "sd";
-
-
-$query = ' INSERT INTO `fd54ymxjciorkgy4`.`hope`
-(`name`)
-VALUES
-("jjj")';
- mysql_query($query);
-
-echo "sd";
-
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 
 
 
