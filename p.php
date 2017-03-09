877363ae3281d59f1c12ad52ed59420f78df97c9
maxpalpal <palestinianpy@gmail.com>
2017-03-09 12:29:32 +0200

    Update p.php

--- p.php
+++ p.php
@@ -1,10 +1,13 @@
-
 <?php
-echo "sdsd";
-$link = mysql_connect('y0nkiij6humroewt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'mz7y3tb1ahil8uyc', 'nk9hj09j0ywgzrac');
-if (!$link) {
-    die('Could not connect: ' . mysql_error());
-}
-echo 'Connected successfully';
-mysql_close($link);
-?>
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
+
+echo $conn;
+?
