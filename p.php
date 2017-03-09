243dfdf3d399917e9eaa7f9508be19b22593c35d
maxpalpal <palestinianpy@gmail.com>
2017-03-09 12:27:01 +0200

    Update p.php

--- p.php
+++ p.php
@@ -1,7 +1,7 @@
 
 <?php
 echo "sdsd";
-$link = mysql_connect('localhost', 'mysql_user', 'mysql_password');
+$link = mysql_connect('y0nkiij6humroewt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'mz7y3tb1ahil8uyc', 'nk9hj09j0ywgzrac');
 if (!$link) {
     die('Could not connect: ' . mysql_error());
 }
